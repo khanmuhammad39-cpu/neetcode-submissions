0117867f9088e70e1944f6435816812c3126fb47
Muhammad Nawaz Khan <khanmuhammad39@gmail.com>
2026-06-07 20:22:37 -0500

    Add: sqrtx - submission-2

---
+++ Data Structures & Algorithms/sqrtx/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        l = 0
+        r = x
+        res = 0
+
+        while l <= r:
+            m = l + (r - l) // 2
+            if m*m > x:
+                r = m - 1
+            elif m*m < x:
+                res = m
+                l = m + 1
+            else:
+                return m
+        
+        return res
+
+        
